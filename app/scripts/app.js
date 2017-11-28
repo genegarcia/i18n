944debdacf295e025c6952641db2a0b62dba4a24
¨genesis <geny.462@gmail.com>
2017-11-28 00:26:35 -0300

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,144 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name translateApp
+ * @description
+ * # translateApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('translateApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'pascalprecht.translate',// angular-translate
+    'tmh.dynamicLocale'// angular-dynamic-locale
+  ])
+  .service('LocaleService', function($translate, LOCALES, $rootScope, tmhDynamicLocale) {
+      'use strict';
+      // PREPARING LOCALES INFO
+      var localesObj = LOCALES.locales;
+
+      // locales and locales display names
+      var _LOCALES = Object.keys(localesObj);
+      if (!_LOCALES || _LOCALES.length === 0) {
+      console.error('There are no _LOCALES provided');
+      }
+      var _LOCALES_DISPLAY_NAMES = [];
+      _LOCALES.forEach(function (locale) {
+      _LOCALES_DISPLAY_NAMES.push(localesObj[locale]);
+      });
+
+      // STORING CURRENT LOCALE
+      var currentLocale = $translate.proposedLanguage();// because of async loading
+
+      // METHODS
+      var checkLocaleIsValid = function (locale) {
+      return _LOCALES.indexOf(locale) !== -1;
+      };
+
+      var setLocale = function (locale) {
+      if (!checkLocaleIsValid(locale)) {
+        console.error('Locale name "' + locale + '" is invalid');
+        return;
+      }
+      currentLocale = locale;// updating current locale
+
+      // asking angular-translate to load and apply proper translations
+      $translate.use(locale);
+      };
+
+      // EVENTS
+      // on successful applying translations by angular-translate
+      $rootScope.$on('$translateChangeSuccess', function (event, data) {
+      document.documentElement.setAttribute('lang', data.language);// sets "lang" attribute to html
+
+       // asking angular-dynamic-locale to load and apply proper AngularJS $locale setting
+      tmhDynamicLocale.set(data.language.toLowerCase().replace(/_/g, '-'));
+      });
+
+      return {
+      getLocaleDisplayName: function () {
+        return localesObj[currentLocale];
+      },
+      setLocaleByDisplayName: function (localeDisplayName) {
+        setLocale(
+          _LOCALES[
+            _LOCALES_DISPLAY_NAMES.indexOf(localeDisplayName)// get locale index
+            ]
+        );
+      },
+      getLocalesDisplayNames: function () {
+        return _LOCALES_DISPLAY_NAMES;
+      }
+      };
+  })
+  .directive('ngTranslateLanguageSelect', function (LocaleService) { 'use strict';
+
+        return {
+            restrict: 'A',
+            replace: true,
+            template: ''+
+            '<div class="language-select" ng-if="visible">'+
+                '<label>'+
+                    '{{"directives.language-select.Language" | translate}}:'+
+                    '<select ng-model="currentLocaleDisplayName"'+
+                        'ng-options="localesDisplayName for localesDisplayName in localesDisplayNames"'+
+                        'ng-change="changeLanguage(currentLocaleDisplayName)">'+
+                    '</select>'+
+                '</label>'+
+            '</div>'+
+            '',
+            controller: function ($scope) {
+                $scope.currentLocaleDisplayName = LocaleService.getLocaleDisplayName();
+                $scope.localesDisplayNames = LocaleService.getLocalesDisplayNames();
+                $scope.visible = $scope.localesDisplayNames &&
+                $scope.localesDisplayNames.length > 1;
+
+                $scope.changeLanguage = function (locale) {
+                    LocaleService.setLocaleByDisplayName(locale);
+                };
+            }
+        };
+    })
+  .constant('LOCALES', {
+    'locales': {
+      'es_CL': 'Espanol',
+      'en_US': 'English',
+      'pt_BR': 'Portugues'
+    },
+    'preferredLocale': 'en_US'
+  })
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .config(function ($translateProvider) {
+    $translateProvider.useMissingTranslationHandlerLog();
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'resources/locale-',// path to translations files
+        suffix: '.json'// suffix, currently- extension of the translations
+    });
+    $translateProvider.preferredLanguage('en_US');// is applied on first load
+    $translateProvider.useLocalStorage();// saves selected language to localStorage
+  })
+  .config(function (tmhDynamicLocaleProvider) {
+    tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name translateApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the translateApp
+ */
+angular.module('translateApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
